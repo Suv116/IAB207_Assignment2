6b5f755ced8728073d39b481c3bdc8d83d98870d
Suv116 <Reillyb04@gmail.com>
2025-09-22 13:58:31 +1000

    different front end pages for different user types

--- a2_starter_code/projectfile/website/views.py
+++ a2_starter_code/projectfile/website/views.py
@@ -20,21 +20,25 @@ def details():
 
 # Create event page
 @main_bp.route('/create-event')
+@login_required
 def create_event():
     return render_template('CreateEvent.html')
 
 # Update event page
 @main_bp.route('/update-event')
+@login_required
 def update_event():
     return render_template('UpdateEvent.html')
 
 # Upcoming events page
 @main_bp.route('/upcoming-event')
+@login_required
 def upcoming_event():
     return render_template('UpcomingEvent.html')
 
 # Event history page
 @main_bp.route('/history')
+@login_required
 def history():
     return render_template('History.html')
 
